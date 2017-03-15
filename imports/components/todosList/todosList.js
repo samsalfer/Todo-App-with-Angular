5fabde5c2043a95a5afde558eabaeca81a0ef243
Sala Fernandez <SalaSamuel46K@dmsas.sda.sas.junta-andalucia.es>
2017-03-15 13:52:36 +0100

    Punto estable en el proyecto - Listar, borrar, crear y tachar

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -3,24 +3,55 @@
  */
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+
 import template from './todosList.html';
 
 class TodosListCtrl {
-    constructor() {
-        this.tasks = [{
-            text: 'This is task 1'
-        }, {
-            text: 'This is task 2'
-        }, {
-            text: 'This is task 3'
-        }];
+    constructor($scope) {
+        $scope.viewModel(this);
+
+        this.helpers({
+            tasks() {
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
+            }
+        })
+    }
+
+    addTask(newTask) {
+        // Insert a task into the collection
+        Tasks.insert({
+            text: newTask,
+            createdAt: new Date
+        });
+
+        // Clear form
+        this.newTask = '';
+    }
+
+    setChecked(task) {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(task._id, {
+            $set: {
+                checked: !task.checked
+            },
+        });
+    }
+
+    removeTask(task) {
+        Tasks.remove(task._id);
     }
 }
+
 
 export default angular.module('todosList', [
     angularMeteor
 ])
     .component('todosList', {
         templateUrl: 'imports/components/todosList/todosList.html',
-        controller: TodosListCtrl
+        controller: ['$scope', TodosListCtrl]
     });
