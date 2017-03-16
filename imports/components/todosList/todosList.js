1d0f71e3a887c5f7a1c64591008822d6178c2930
Sala Fernandez <SalaSamuel46K@dmsas.sda.sas.junta-andalucia.es>
2017-03-16 13:23:52 +0100

    Añadido usuarios, seguridad y publicaciones - Paso 10 Publish and Subscribe

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -4,12 +4,16 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { Tasks } from '../../api/tasks.js';
+import { Meteor } from 'meteor/meteor';
+
 
 import template from './todosList.html';
 
 class TodosListCtrl {
     constructor($scope) {
         $scope.viewModel(this);
+
+        this.subscribe('tasks');
 
         this.hideCompleted = false;
 
@@ -35,6 +39,9 @@ class TodosListCtrl {
                         $ne: true
                     }
                 }).count();
+            },
+            currentUser() {
+                return Meteor.user();
             }
 
 
@@ -44,10 +51,7 @@ class TodosListCtrl {
 
     addTask(newTask) {
         // Insert a task into the collection
-        Tasks.insert({
-            text: newTask,
-            createdAt: new Date
-        });
+        Meteor.call('tasks.insert', newTask);
 
         // Clear form
         this.newTask = '';
@@ -55,18 +59,17 @@ class TodosListCtrl {
 
     setChecked(task) {
         // Set the checked property to the opposite of its current value
-        Tasks.update(task._id, {
-            $set: {
-                checked: !task.checked
-            },
-        });
+        Meteor.call('tasks.setChecked', task._id, !task.checked);
+
     }
 
     removeTask(task) {
-        Tasks.remove(task._id);
+        Meteor.call('tasks.remove', task._id);
     }
 
-
+    setPrivate(task) {
+        Meteor.call('tasks.setPrivate', task._id, !task.private);
+    }
 
 }
 
---
+++ imports/startup/accounts-config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by SalaSamuel46K on 16/03/2017.
+ */
+import { Accounts } from 'meteor/accounts-base';
+
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY',
+});
